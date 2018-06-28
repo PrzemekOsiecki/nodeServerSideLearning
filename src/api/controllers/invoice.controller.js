a7eb88740fb00fd5e69bb2f088a48cbe710a79a7
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2018-06-28 11:00:55 +0200

    Create endpionts with Swagger documentation

---
+++ src/api/controllers/invoice.controller.js
@@ -0,0 +1,99 @@
+import Invoice from '../models/invoice.model';
+import Joi from 'joi';
+import HttpStatus from 'http-status-codes';
+
+export default {
+    
+    findAll(req, res, next) {
+       Invoice.find().then(invoices => res.json(invoices))
+       .catch(err => 
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(err))
+            .catch(err => 
+                err.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .json(err));
+    },
+
+    findOne(req, res){
+        let {id} = req.params;
+        Invoice.findById(id)
+            .then(invoice => {
+                if(!invoice) {
+                    return res.status(HttpStatus.NOT_FOUND)
+                        .json({
+                            err: 'Invoice could not found for given id'
+                        });
+                }
+                return res.json(invoice);
+            })
+            .catch(err => 
+                err.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .json(err));
+    },
+
+    create(req, res) {
+        const schema = Joi.object().keys({
+            item: Joi.string().required(),
+            date: Joi.date().required(),
+            due: Joi.date().required(),
+            qty: Joi.number().integer().required(),
+            tax: Joi.number().optional(),
+            rate: Joi.number().optional()
+        });
+        
+        const {error, value} = Joi.validate(req.body, schema);
+
+        if(error && error.details) {
+            return res
+                .status(HttpStatus.BAD_REQUEST)
+                .json(error); //400
+        }
+
+        Invoice.create(value)
+        .then(invoices => res.json(invoices))
+        .catch(err =>
+             res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+             .json(err));
+    },
+
+    update(req, res) {
+        let {id} = req.params;
+
+        const schema = Joi.object().keys({
+            item: Joi.string().optional(),
+            date: Joi.date().optional(),
+            due: Joi.date().optional(),
+            qty: Joi.number().integer().optional(),
+            tax: Joi.number().optional(),
+            rate: Joi.number().optional()
+        });
+        
+        const {error, value} = Joi.validate(req.body, schema);
+
+        if(error && error.details) {
+            return res
+                .status(HttpStatus.BAD_REQUEST)
+                .json(error);
+        }
+
+        Invoice.findByIdAndUpdate({_id: id}, value, {new: true})
+            .then(invoice => res.json(invoice)) //new:true I want to see updated record in response
+            .catch(err =>
+                err.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .json(err));
+    },
+
+    delete(req, res) {
+        let {id} = req.params;
+        Invoice.findByIdAndRemove(id)
+            .then(invoice => {
+                if(!invoice) {
+                    return res.status(HttpStatus.NOT_FOUND)
+                        .json({err: 'Invoice could not delete for given id'});
+                }
+                return res.json(invoice);
+            })
+            .catch(err => err.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err));
+    }
+
+}
---
+++ src/app.js
@@ -0,0 +1,65 @@
+//const express = require('express');
+import express from 'express';
+import {router} from './config/routes'
+
+import mongoose from 'mongoose';
+import logger from 'morgan';
+
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from '../src/config/swagger.json';
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/invoice-builder');
+//                  host            / db name
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json({extended:true})); //BodyParser 
+app.use(express.urlencoded({extended:true})); //parsing url parameters 
+
+app.use(logger('dev')); //other option is for example: combined
+
+app.use('/api', router);
+
+app.use('/api-docs', swaggerUi.serve, 
+swaggerUi.setup(swaggerDocument, {
+    explorer: true
+}));
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.message = 'Invalid route';
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'Welcome to GET request handler'
+    });
+});
+
+const invoices = [
+    {_id: 1, name: 'Amazing Product1', qty: 10, date: new Date()},
+    {_id: 2, name: 'Some Product2', qty: 10, date: new Date()},
+    {_id: 3, name: 'Petarda Product3', qty: 10, date: new Date()}
+];
+
+app.get('/invoices', (req, res) => {
+    res.json(invoices);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+});
